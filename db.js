cd5adb31e7cccb411e12f698dd2ae013fae337b7
Pedro Zanatta <pedrohenriquezanattadeoliveira@gmail.com>
2025-10-02 09:56:47 -0300

    database connected and files created

--- db.js
+++
@@ -1,9 +0,0 @@
-// Carrega as variáveis de ambiente do arquivo .env
-require ('dotenv').config();
-
-// Importa o mongoose para conectar ao MongoDB
-const mongoose = require('mongoose');
-
-// Obtém a URI do MongoDB a partir das variáveis de ambiente e acessa ela
-const MONGO_URI = process.env.MONGO_URI;
-
